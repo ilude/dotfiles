0b931c719b853fe663cfa42dce867c8a2e9312e7
Mike Glenn <219478+ilude@users.noreply.github.com>
2023-09-11 21:46:43 -0400

    entirely too complicated

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -1,12 +1,51 @@
 #!/bin/bash
 
+OS=$(uname -s | tr A-Z a-z)
+PACKAGES="make zsh zsh-autosuggestions zsh-syntax-highlighting"
+echo $OS
+case $OS in
+  linux)
+    source /etc/os-release
+    echo $ID
+    case $ID in
+      debian|ubuntu|mint)
+        if [[ "$EUID" -ne 0 ]]; then
+          sudo apt -y install $PACKAGES
+        else
+          apt -y install $PACKAGES
+        fi
+        ;;
+      alpine)
+        if [[ "$EUID" -ne 0 ]]; then
+          sudo apk add $PACKAGES shadow
+          echo "auth        sufficient  pam_rootok.so" | sudo tee /etc/pam.d/chsh
+        else
+          apk add $PACKAGES
+        fi
+        ;;
+      fedora|rhel|centos)
+        if [[ "$EUID" -ne 0 ]]; then
+          sudo yum install $PACKAGES
+        else
+          yum install $PACKAGES
+        fi
+        ;;
+      *)
+        echo -n "unsupported linux distro"
+        ;;
+    esac
+  ;;
+
+  *)
+    echo -n "unsupported OS"
+    ;;
+esac
+
 # check if we are in proxmox
 if [[ "$EUID" -ne 0 ]]; then
-  sudo apt -y install make zsh zsh-autosuggestions zsh-syntax-highlighting
   echo Setting $(whoami) shell to $(which zsh)
   sudo chsh -s $(which zsh) $(whoami)
 else
-  apt -y install make zsh zsh-autosuggestions zsh-syntax-highlighting
   echo Setting $(whoami) shell to $(which zsh)
   chsh -s $(which zsh) $(whoami)
 fi
